2b14e92a4ea294132929b64e330f2dbc8a1e3b86
Colin Sandlin <csandlin010@gmail.com>
2019-07-19 12:04:35 -0500

    added Chicken, Duck, Pig

---
+++ src/Models/Animals/Duck.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Animals
+{
+    public class Duck : IResource, IPecking, IEggProducing, IFeatherProducing
+    {
+
+        private Guid _id = Guid.NewGuid();
+
+        private int _eggProduced = 6;
+        private double _feathersProduced = .75;
+
+        private string _shortId
+        {
+            get
+            {
+                return this._id.ToString().Substring(this._id.ToString().Length - 6);
+            }
+        }
+
+        public string Type { get; } = "Duck";
+        public double SeedPerDay { get; set; }
+
+        // Methods
+
+        public override string ToString()
+        {
+            return $"Duck {this._shortId}. I'm on Quack!";
+        }
+
+        public void Peck()
+        {
+            Console.WriteLine($"Duck {this._shortId} just ate {this.SeedPerDay}kg of seed");
+        }
+
+        public int Gather()
+        {
+            return _eggProduced;
+        }
+
+        public double Pluck()
+        {
+            return _feathersProduced;
+        }
+    }
+}
