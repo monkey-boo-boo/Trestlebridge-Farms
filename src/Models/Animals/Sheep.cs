bc53f7693d01db8f124f1eb2db854089cc5dcd26
Carly Bergthold <carlybergthold@gmail.com>
2019-07-19 12:04:28 -0500

    goat and sheep added, integrated terminal for debugging

---
+++ src/Models/Animals/Sheep.cs
@@ -0,0 +1,31 @@
+using System;
+using Trestlebridge.Interfaces;
+
+namespace Trestlebridge.Models.Animals
+{
+    public class Sheep : IResource, IGrazing, IMeatProducing
+    {
+        private Guid _id = Guid.NewGuid();
+        private double _meatProduced = 5;
+
+        public string Type { get; set; }
+
+        public double GrassPerDay { get; set; } = 4;
+
+        private string _shortId {
+            get {
+                return this._id.ToString().Substring(this._id.ToString().Length - 6);
+            }
+        }
+
+        public double Butcher()
+        {
+            return _meatProduced;
+        }
+
+        public void Graze()
+        {
+            Console.WriteLine($"Sheep {this._shortId} just ate {this.GrassPerDay}kg of grass");
+        }
+    }
+}
